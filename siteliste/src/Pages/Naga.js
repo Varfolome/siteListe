05de0cc3b0a57956650eeda66c07c42a4576b32a
Tomati <dopedup@mail.ru>
2020-11-19 19:54:52 +0100

    Started work on page switching v1

---
+++ siteliste/src/Pages/Naga.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class Naga extends React.Component {
+
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>This is Naga!</h1>
+      </div>
+    );
+  }
+}
