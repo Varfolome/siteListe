1b9efdc60b67f889b6562d069251b2f85c284ad0
Tomati <dopedup@mail.ru>
2020-12-03 08:38:55 +0100

    Added scroll counter and some text to test in FAQ

--- siteliste/src/StaticPageElements/Menu/MenuList.js
+++ siteliste/src/StaticPageElements/Menu/MenuList.js
@@ -38,7 +38,10 @@ export default class MenuList extends React.Component {
 
   handleScroll = () => {
     let scrolled = document.documentElement.scrollTop; // returns scrolled distance
-    let menuControl = document.querySelector('.menu-control'); // refers to menu pannel
+    let scrollH = document.documentElement.scrollHeight - window.innerHeight;
+    let menuControl = document.querySelector('.menu-scroll-control'); // refers to menu pannel
+    let w = (scrolled / scrollH) * 100 + "%";
+    document.documentElement.style.setProperty("--snake-scroll-width", w);
     /*
     'uncomment to compare scrolled and height of pannel + header'
     let h = menuControl.clientHeight + 120;
@@ -79,7 +82,7 @@ export default class MenuList extends React.Component {
   render() {
 
     // start of a block that dynamically make difference between desktop and mobile styles for menu
-    
+
     const windowWidth = window.innerWidth;
     let ulStyle = {};
     let burgerStyle = {};
@@ -108,7 +111,8 @@ export default class MenuList extends React.Component {
 
 
     return(
-      <div className="menu" ref={this.nav}>
+      <div className="menu">
+      <div className="menu-scroll-control">
         <div className="menu-control">
         <button onClick={this.Open} className="burger" style={burgerStyle}>click me</button>
         <ul className="menu-list" style={ulStyle}>
@@ -120,6 +124,8 @@ export default class MenuList extends React.Component {
           <li className="menu-element" onClick={this.showPage.bind(this,"Partenaires")}>Partenaires</li>
           <li className="menu-element" onClick={this.showPage.bind(this,"FAQ")}>FAQ</li>
         </ul>
+        </div>
+        <div className="snake-scroller"></div>
         </div>
         <ControlPages pageToShow={this.state.pageToShow} />
       </div>
