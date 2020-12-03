50f288df7169d5f89b72dfce088e084290f8cf3c
Lovali <eloise.bonnet@telecom-sudparis.eu>
2020-12-03 09:48:18 +0100

    Ajout de la page d'accueil

--- siteliste/src/Pages/Naga.js
+++ siteliste/src/Pages/Naga.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './PagesStyles/Naga.css';
 
 export default class Naga extends React.Component {
 
@@ -8,8 +9,18 @@ export default class Naga extends React.Component {
 
   render() {
     return(
-      <div>
-        <h1>This is Naga!</h1>
+    	<div>
+      <div class="Titre">
+        <h1>Vous entrez en territoire Naga...</h1>
+        <p>(Insérer ici une photo de tous les membres de la liste ensemble ?)</p>
+      </div>
+      <div class="Reseaux">
+      	<h2>Suivre les Nagas sur les réseaux</h2>
+      	<p>Pour connaître toutes les actualités des Nagas, n'hésitez pas à nous suivre sur nos réseaux :<br />
+      	Facebook : <a href="https://www.facebook.com/Naga.ListeBDE.2021/">Naga - Liste BDE 2021</a><br />
+      	Instagram : <a href="https://www.instagram.com/naga.2021/?igshid=qydk0gnm5ao2">@naga.2021</a>
+      	</p>
+      </div>
       </div>
     );
   }
