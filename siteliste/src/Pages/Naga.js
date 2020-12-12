89011c1fa0ed6e5ea901f8234e5815af519f2ee0
Lovali <eloise.bonnet@telecom-sudparis.eu>
2020-12-12 16:38:52 +0100

    Changement de police pour Naga

--- siteliste/src/Pages/Naga.js
+++ siteliste/src/Pages/Naga.js
@@ -10,11 +10,11 @@ export default class Naga extends React.Component {
   render() {
     return(
     	<div>
-      <div class="Titre">
+      <div className="Titre">
         <h1>Vous entrez en territoire Naga...</h1>
         <p>(Insérer ici une photo de tous les membres de la liste ensemble ?)</p>
       </div>
-      <div class="Reseaux">
+      <div className="Reseaux">
       	<h2>Suivre les Nagas sur les réseaux</h2>
       	<p>Pour connaître toutes les actualités des Nagas, n'hésitez pas à nous suivre sur nos réseaux :<br />
       	Facebook : <a href="https://www.facebook.com/Naga.ListeBDE.2021/">Naga - Liste BDE 2021</a><br />
