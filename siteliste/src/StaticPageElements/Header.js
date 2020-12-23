927e3a865a26378f739d387010764eca5107b723
Tomati <dopedup@mail.ru>
2020-12-23 08:20:53 +0100

    Added logo and changed burger. Modified headers styles for adaptiveness.

--- siteliste/src/StaticPageElements/Header.js
+++ siteliste/src/StaticPageElements/Header.js
@@ -17,6 +17,7 @@ export default function Header() {
   return(
     <div className="header">
         <HeaderBloom text="Naga" />
+        <div className="naga-logo"></div>
         <div className="apearing-description">
         <AppearText appearText="Naga est la meilleure liste de TSP. Nous avons notre chambre secret dans le sous sol de TSP ou nous nous cachons du reste du monde. Mais on est partout. On est meme dant ton jardin mdr. On est confine chez toi. Toutes les associations obeissent a nous. Bonne chance..." />
         </div>
