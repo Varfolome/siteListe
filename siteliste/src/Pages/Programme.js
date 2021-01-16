17b442759bed3c21704c14012b4b39f9ce6414bf
Tomati <dopedup@mail.ru>
2021-01-16 11:44:20 +0100

    Made a menu container for programme

--- siteliste/src/Pages/Programme.js
+++ siteliste/src/Pages/Programme.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Purpose from '../WidgetsAndElements/Purpose';
+import MenuContainer from '../WidgetsAndElements/MenuContainer';
 import test from '../WidgetsAndElements/photos/instagram.png';
 import './PagesStyles/Programme.css';
 
@@ -14,11 +15,12 @@ export default class Programme extends React.Component {
     let generalPurposes = [[test, "testafsgergergererh", "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."], [test, "lol", "sdgsdgsgshshsfh"]];
     let specificPurposes = [[test,"Soirée","L'objectif est de rendre l'expérience des soirées à l'INT inoubliable en visant toujours plus haut, toujours plus gros. En plus des traditionnels Club B et soirées organisées par le BDE chaque année on organiserait une sortie sur Paris pour la Technoparade et une sorte de Dîner Presque Parfait de la soirée en partenariat avec les autres écoles de l'Institut Polytechnique de Paris où il serait promis au vainqueur gloire et prestige éternels."],
     [test, "Séculog","Faire en sorte que les étudiants puissent profiter au maximum des événements organisés en toute sécurité avec une organisation optimale et un staffing sérieux."],
-    [test, "Com","Mettre au courant les élèves des événements et fêtes organisés sur le campus pour leur donner envie de participer. C’est ainsi que le campus continuera à vivre, surtout en cette période difficile. Ne pas laisser de coté les élèves internationaux. Reprendre le menu de la semaine sur campus TMSP"]
+    [test, "Com","Mettre au courant les élèves des événements et fêtes organisés sur le campus pour leur donner envie de participer. C’est ainsi que le campus continuera à vivre, surtout en cette période difficile. Ne pas laisser de coté les élèves internationaux. Reprendre le menu de la semaine sur campus TMSP"],
+    [test,"Prévention","La prévention de cette année a pour but, si la situation sanitaire le permet, d'enseigner une nouvelle façon de s'amuser en soirée tout en respectant sa santé et ses envies. La prévention sera également consciente des potentiels excès et débordements qui peuvent survenir, et fera tout son possible pour en minimiser les effets indésirables."]
     ];
 
     let generalPurposesJSX = generalPurposes.map((generalPurposeProps) => <li className="general-purposes-list-element" key={generalPurposeProps[1]}><Purpose purposeImg={generalPurposeProps[0]} purposeName={generalPurposeProps[1]} purposeDescription={generalPurposeProps[2]}/><br/><hr/><br/></li>);
-    let specificPurposesJSX = specificPurposes.map((specificPurposeProps) => <li className="specific-purposes-list-element" key={specificPurposeProps[1]}><Purpose purposeImg={specificPurposeProps[0]} purposeName={specificPurposeProps[1]} purposeDescription={specificPurposeProps[2]}/><br/><hr/><br/></li>);
+    let specificPurposesJSX = specificPurposes.map((specificPurposeProps) => <div className="specific-purposes-element" key={specificPurposeProps[1]}><Purpose purposeImg={specificPurposeProps[0]} purposeName={specificPurposeProps[1]} purposeDescription={specificPurposeProps[2]}/></div>);
 
     return(
       <div className="programme">
@@ -33,11 +35,7 @@ export default class Programme extends React.Component {
         <br/>
         <h1>Nos objectifs spécifiques:</h1>
         <br/>
-        <div className="specific-purposes">
-          <ul className="specific-purposes-list">
-            {specificPurposesJSX}
-          </ul>
-        </div>
+        <MenuContainer control="Soirée Séculog Com Prévention" content={specificPurposesJSX}/>
       </div>
     );
   }
---
+++ siteliste/src/WidgetsAndElements/MenuContainer.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './Styles/MenuContainer.css';
+
+export default class MenuContainer extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {currentRenderingContentIndex: 0};
+  }
+
+  showNewSection(newSectionNumber) {
+    let idPrev = "";
+    let idNext = "";
+    idPrev += this.state.currentRenderingContentIndex;
+    idNext += newSectionNumber;
+    document.getElementById(idPrev).style="background: gray";
+    document.getElementById(idNext).style="background: black";
+    this.setState(state =>({
+      currentRenderingContentIndex: newSectionNumber
+    }));
+  }
+
+  menuControlItems = this.props.control.split(" ");
+  i = 0;
+  menuControlItemsJSX = this.menuControlItems.map((controlItem) => <div key={controlItem} className="menu-container-control-element-wrapper rounded"><li id={this.i} className="menu-container-control-element" style={this.i === 0 ? {background: "black"} : {background: "gray"}} onClick={this.showNewSection.bind(this,this.i++)}>{controlItem}</li></div>);
+
+
+  render() {
+
+
+      return(
+        <div className="menu-container-wrapper">
+          <div className="menu-container">
+            <div className="menu-container-control-wrapper">
+              <ul className="menu-container-control">
+                {this.menuControlItemsJSX}
+              </ul>
+            </div>
+            <div className="menu-container-inner">
+              {this.props.content[this.state.currentRenderingContentIndex]}
+            </div>
+          </div>
+        </div>
+      );
+  }
+}
