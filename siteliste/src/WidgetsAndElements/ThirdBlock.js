88f60bed065a6603c1a83839a535cd9cd39cd8fb
Tomati <dopedup@mail.ru>
2021-01-17 10:49:25 +0100

    Finished current mdp traduction

--- siteliste/src/WidgetsAndElements/ThirdBlock.js
+++ siteliste/src/WidgetsAndElements/ThirdBlock.js
@@ -15,38 +15,35 @@ import tres from '../photos/img_mdp/tres.jpg'
 
 export default function ThirdBlock(props) {
 
+  let textPrev="La prévention de cette année a pour but, si la situation sanitaire le permet, d'enseigner une nouvelle façon de s'amuser en soirée tout en respectant sa santé et ses envies. La prévention sera également consciente des potentiels excès et débordements qui peuvent survenir, et fera tout son possible pour en minimiser les effets indésirables.";
+  let textRits="Satisfaire vos estomacs en vous faisant découvrir nos recettes tout en s'adaptant à vos goûts. Assurer des stands de nourriture occasionnellement afin d'avoir un petit remontant pendant sa journée.";
+  let textRe="";
+  let textSecret="Le secrétariat, le pôle de l'ombre ! En tant que secrétaires, nous participons très activement à la vie de la liste, en réalisant les compte-rendus de réunion, en s'occupant des documents administratifs, des ressources en ligne et nous aidons tous les autres pôles !";
+  let textTres="Notre objectif est d'optimiser au maximum le budget de la trésorerie en l'utilisant de manière judicieuse. Un budget bien géré pour un campus plus dynamique. Rattrapons les dégâts de la COVID-19 !";
+
+  if(props.lang === "english") {
+    textPrev="The goal of this year's prevention is, if the health situation permits, to teach a new way of having fun in parties while respecting one's health and desires. Prevention will also be aware of the potential excesses and overflows that can occur, and will do everything possible to minimize the undesirable effects.";
+    textRits="Satisfy your stomachs by making you discover our recipes while adapting to your tastes. Provide food stalls occasionally to have a little pick-me-up during your day.";
+    textRe="";
+    textSecret="The secretariat, the department of the shadow! As secretaries, we take a very active part in the life of the list, by realizing reports of meetings, taking care of administrative documents and online resources and we also help all the other departments!";
+    textTres="Our aim is to optimize the treasury budget to its maximum by using it wisely. A well-managed budget for a more dynamic campus. Let's catch the damage of the COVID-19!";
+  }
+
   let blockPrev =   <Block_Pole nbr="1" title="Prévention" imgone={prev}
       textname="Eloi"
-      text="La prévention, c'est le couteau suisse de ta soirée. T'as besoin d'alcool ? On en a !
-      Tu veux te rincer la bouche parce que tu t'apprêtes à pécho, ou parce que t'as soif ? On est là !
-      Tu veux prendre une banane pour tanker car t'as CF demain ? On en a !
-      Tu veux parler à quelqu'un pour une quelconque raison ? On est là !
-      Enfin, plus ou moins... Dans tous les cas, rien que pour toi, nous sommes là pour
-      te faire vivre tes meilleures soirées !" />;
+      text={textPrev} />;
   let blockRits =   <Block_Pole nbr="2" title="Rits" imgone={resporits} imgtwo={rits}
       textname="Ella et Aya"
-      text="Comme le dit si bien le dicton <<bonne mangeoire fait bonne fête>>
-      c'est la bonne cuisine qui fait la bonne fête ! Vous ne serez jamais mieux servis que par nous,
-      le pôle RITZ, puisque nous vivons d'amour, d'eau fraîche, d'entraide, et de bons petits plats !" />;
+      text={textRits} />;
   let blockRE = <Block_Pole nbr="2" title="RE" imgone={respore} imgtwo={re}
     textname="Louri et Belhassen"
-    text="Toujours à l’affût d’opportunités, de faire valoir le bien-être et l’avenir des étudiants
-     auprès des entreprises, le pôle RE fera tout son possible pour régaler le campus tout en
-     répondant aux attentes de ses partenaires du monde de l’entreprise. De l’organisation d’événements
-     ludique à buts professionnels à la proposition d’offres alléchantes, nous entendons mettre en
-     relation les étudiants du campus et les entreprises de la manière la plus adaptée possible.
-     Prêts à démarcher jusqu’au bout du monde, on arrive pour vous offrir le meilleur de nos poignées de mains." />;
+    text={textRe} />;
   let blockSecret = <Block_Pole nbr="2" title="Secrétariat" imgone={resposecret} imgtwo={secret}
     textname="Kenza et Pierre"
-    text="Le secrétariat, le pôle de l'ombre ! En tant que secrétaires,
-    nous participons très activement à la vie de la liste, en réalisant les compte-rendus de réunion,
-    en s'occupant des documents administratifs, des ressources en ligne et nous aidons tous les autres pôles !" />;
+    text={textSecret} />;
   let blockTres = <Block_Pole nbr="2" title="Trésorerie" imgone={respotres} imgtwo={tres}
     textname="Fatima et Jahed"
-    text="Le pôle trésorerie s'occupe des fonds afin de permettre à tous les pôles de mener
-    à bien leurs projets. Nous nous occupons des entrées et des sorties d'argent tout ça pour
-     que tu puisses faire la mala à nos stands ou encore (et surtout) à nos soirées.
-     Bref, notre seul objectif, c'est de brasser et t'as capté qu'on te parle pas de natation." />;
+    text={textTres} />;
 
   let thirdBlock = <div className="grille">
   <div className="lign">
